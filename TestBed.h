48b05eb735a5cf354e3f62abdd28c574e1029862
Zachary Tay <zacharytay1994@gmail.com>
2020-11-02 11:22:23 +0800

    scene and test bed, clean up main

---
+++ TestBed.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void TestBed_Init();
+void TestBed_Update(const float dt);
+void TestBed_Exit();
---
+++ TestScene1.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void TestScene1_Init();
+void TestScene1_Update(const float dt);
+void TestScene1_Exit();
