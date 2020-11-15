de6c11af1cf30b5e142ade64b4e78b91dc3d547d
rytenow <ryteray@gmail.com>
2020-11-15 14:10:25 +0800

    house and enemy HP, enemyHP not to scale(yet)

--- TestScene1.h
+++ TestScene1.h
@@ -21,6 +21,8 @@ float house_posY;
 float house_SizeX;
 float house_SizeY;
 int tempHouseSprite_id;
+int tempHouseHP_spriteId;
+float houseHP;
 void KeyInputAssign();	//Checks for keyboard input 
 
 
